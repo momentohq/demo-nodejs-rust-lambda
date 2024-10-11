61f90b6c15cb27e5a9f08648bca0b537f85f7f98
Chris Price <chris@momentohq.com>
2024-10-10 20:56:43 -0700

    feat: milestone3; added initial momento cache facade

--- lambdas/rust-workspace/momento_cache_facade/src/bin/hello.rs
+++ lambdas/rust-workspace/momento_cache_facade/src/bin/hello.rs
@@ -1,9 +1,16 @@
-use momento_cache_facade::MomentoCacheFacade;
+use log::info;
+use momento_cache_facade::{initialize_logging, MomentoCacheFacade};
 
-fn main() {
-    println!("Hello, world!");
-    let foo = "Hello";
-    let cache_facade = MomentoCacheFacade::new(foo.to_string(), 42);
-    println!("Constructed an instance of the cache facade struct! {:?}", cache_facade);
-    println!("The foo field of the cache facade struct is: {}", cache_facade.get_foo());
+#[tokio::main]
+async fn main() {
+    dotenv::from_path("../../../.env").expect("Failed to load .env file");
+    let momento_cache_name = dotenv::var("MOMENTO_CACHE_NAME").expect("Failed to get MOMENTO_CACHE_NAME from .env file");
+    let momento_api_key = dotenv::var("MOMENTO_API_KEY").expect("Failed to get MOMENTO_API_KEY from .env file");
+    let log_level = dotenv::var("LOG_LEVEL").expect("Failed to get LOG_LEVEL from .env file");
+    initialize_logging(&log_level);
+    let cache_facade = MomentoCacheFacade::new(momento_api_key, momento_cache_name);
+    info!("Storing value in cache");
+    cache_facade.store("foo", "FOO").await;
+    let value = cache_facade.retrieve("foo").await;
+    info!("Retrieved value from cache: {:?}", value);
 }
--- lambdas/rust-workspace/momento_cache_facade/src/lib.rs
+++ lambdas/rust-workspace/momento_cache_facade/src/lib.rs
@@ -1,19 +1,63 @@
+use std::time::Duration;
+use log::{debug, info, warn};
+use momento::cache::{configurations, GetResponse};
+use momento::{CacheClient, CredentialProvider};
+
+
+pub fn initialize_logging(log_level: &str) {
+    let mut default_builder = colog::default_builder();
+    let log_builder = match log_level {
+        "debug" => default_builder.filter_level(log::LevelFilter::Debug),
+        "info" => default_builder.filter_level(log::LevelFilter::Info),
+        _ => default_builder.filter_level(log::LevelFilter::Info),
+    };
+    // this logger is initialized globally for the Rust process. In an
+    // AWS Lambda environment, the initialized state can persist
+    // across lambda invocations, so we need to handle the case where
+    // it has already been initialized
+    match log_builder.try_init() {
+        Ok(_) => info!("Logging initialized"),
+        Err(e) => warn!("Failed to initialize logging; this may just indicate that logging is already initialized (e.g. in a warm Lambda container): {}", e),
+    }
+}
+
 #[derive(Debug)]
 pub struct MomentoCacheFacade {
-    foo: String,
-    bar: u32
+    cache_name: String,
+    cache_client: CacheClient,
 }
 
 impl MomentoCacheFacade {
-    pub fn new(foo: String, bar: u32) -> MomentoCacheFacade {
+    pub fn new(momento_api_key: String, cache_name: String) -> MomentoCacheFacade {
         MomentoCacheFacade {
-            foo,
-            bar
+            cache_name,
+            cache_client: CacheClient::builder()
+                .default_ttl(Duration::from_secs(60 * 10))
+                .configuration(configurations::Laptop::latest())
+                .credential_provider(
+                    CredentialProvider::from_string(momento_api_key)
+                        .expect("Failed to get API key from environment variable"))
+                .with_num_connections(10)
+                .build()
+                .expect("Failed to build cache client"),
         }
     }
 
-    pub fn get_foo(&self) -> String {
-        self.foo.clone()
+    pub async fn store(&self, key: &str, value: &str) {
+        debug!("Rust MomentoCacheFacade storing key {} with value {}", key, value);
+        self.cache_client.set(&self.cache_name, key, value).await.expect("Failed to store value in cache");
+    }
+
+    pub async fn retrieve(&self, key: &str) -> Option<String> {
+        debug!("Rust MomentoCacheFacade retrieving key {}", key);
+        let response = self.cache_client.get(&self.cache_name, key)
+            .await
+            .expect("Failed to retrieve value from cache");
+
+        match response {
+            GetResponse::Hit { value } => Some(value.try_into().expect("Failed to convert value to string")),
+            GetResponse::Miss => None
+        }
     }
 }
 
