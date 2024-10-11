3c63ca4c4fd313f25832faa2cda6f137776ce253
Chris Price <chris@momentohq.com>
2024-10-10 20:56:43 -0700

    feat: added initial momento cache facade

--- lambdas/rust-workspace/momento_cache_facade/src/lib.rs
+++ lambdas/rust-workspace/momento_cache_facade/src/lib.rs
@@ -1,19 +1,53 @@
+use std::time::Duration;
+use log::debug;
+use momento::cache::{configurations, GetResponse};
+use momento::{CacheClient, CredentialProvider};
+
+pub fn initialize_logging(log_level: &str) {
+    match log_level {
+        "debug" => colog::default_builder().filter_level(log::LevelFilter::Debug).init(),
+        "info" => colog::default_builder().filter_level(log::LevelFilter::Info).init(),
+        _ => colog::default_builder().filter_level(log::LevelFilter::Info).init(),
+    }
+}
+
 #[derive(Debug)]
 pub struct MomentoCacheFacade {
-    foo: String,
-    bar: u32
+    cache_name: String,
+    cache_client: CacheClient,
 }
 
 impl MomentoCacheFacade {
-    pub fn new(foo: String, bar: u32) -> MomentoCacheFacade {
+    pub fn new(cache_name: String, momento_api_key: String) -> MomentoCacheFacade {
         MomentoCacheFacade {
-            foo,
-            bar
+            cache_name,
+            cache_client: CacheClient::builder()
+                .default_ttl(Duration::from_secs(60 * 10))
+                .configuration(configurations::InRegion::latest())
+                .credential_provider(
+                    CredentialProvider::from_string(momento_api_key)
+                        .expect("Failed to get API key from environment variable"))
+                .with_num_connections(10)
+                .build()
+                .expect("Failed to build cache client"),
         }
     }
 
-    pub fn get_foo(&self) -> String {
-        self.foo.clone()
+    pub async fn store(&self, key: &str, value: &str) {
+        debug!("Rust MomentoCacheFacade storing key {} with value {}", key, value);
+        self.cache_client.set(&self.cache_name, key, value).await.expect("Failed to store value in cache");
+    }
+
+    pub async fn retrieve(&self, key: &str) -> Option<String> {
+        debug!("Rust MomentoCacheFacade retrieving key {}", key);
+        let response = self.cache_client.get(&self.cache_name, key)
+            .await
+            .expect("Failed to retrieve value from cache");
+
+        match response {
+            GetResponse::Hit { value } => Some(value.try_into().expect("Failed to convert value to string")),
+            GetResponse::Miss => None
+        }
     }
 }
 
